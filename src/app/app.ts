a218996f8e02b06377e1b7beb3cdba1897363547
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-01 02:55:23 -0600

    Add ngx-translate and wire up the translation files with some test content.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('artisan-store-v2');
+  private translate = inject(TranslateService);
+
+  constructor(){
+    this.translate.addLangs(['es', 'en']);
+    this.translate.setFallbackLang('es');
+    this.translate.use('es');
+  }
 }
