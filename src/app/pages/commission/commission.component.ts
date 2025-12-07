2123ee270d7bcf5788a9f49a30dacecb57cfa488
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-07 00:50:15 -0600

    Wire up the commission page to the translation pipe.

--- src/app/pages/commission/commission.component.ts
+++ src/app/pages/commission/commission.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-commission',
-  imports: [TranslateModule],
+  imports: [TranslateModule, NavbarComponent],
   templateUrl: './commission.component.html',
   styleUrl: './commission.component.css'
 })
