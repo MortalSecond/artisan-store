45addfa5cfaa92a6d0a4aee6f9db36e43797fd8e
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-11 03:41:08 -0600

    Make the gallery page functional.

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { PaintingService } from '../../services/painting.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { GalleryItemComponent } from '../../shared/components/gallery/gallery-item.component';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [TranslateModule, GalleryItemComponent],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+  private paintingsService = inject(PaintingService);
 
+  paintings = toSignal(this.paintingsService.getAllPaintings(),{
+    initialValue:[]
+  });
 }
--- src/app/shared/components/gallery/gallery-item.component.ts
+++ src/app/shared/components/gallery/gallery-item.component.ts
@@ -1,13 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { Painting } from '../../models/painting';
+import { DecimalPipe } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-gallery-item',
-  imports: [TranslateModule],
+  imports: [TranslateModule, DecimalPipe, RouterLink],
   templateUrl: './gallery-item.component.html',
   styleUrl: './gallery-item.component.css'
 })
 export class GalleryItemComponent {
   @Input() painting!: Painting;
+  isPaintingInformationVisible = signal(false);
+  isPaintingAvailable = signal(true);
+
+  ngOnInit(){
+    if(this.painting.available == false)
+      this.isPaintingAvailable.set(false);
+  }
+
+  showPaintingInformation(){
+    this.isPaintingInformationVisible.set(true);
+  }
+
+  hidePaintingInformation(){
+    this.isPaintingInformationVisible.set(false);
+  }
+
+
 }
