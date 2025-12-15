fc4d3f94d4a7e76e2b6abaed66a41a86fa0313b9
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-15 13:38:06 -0600

    Update the admin login page to handle expired tokens.

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +9,24 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   private authService = inject(AuthService);
   private router = inject(Router);
+  private route = inject(ActivatedRoute);
 
   username = '';
   password = '';
   isLoading = signal(false);
   errorMessage = signal('');
+
+  ngOnInit(): void {
+    // Check If Redirected Due To Expired Token
+    this.route.queryParams.subscribe(params=>{
+      if (params['expired'] === 'true'){
+        this.errorMessage.set("Tu sesion ha expirado. Inicia sesion de nuevo.");
+      }
+    });
+  }
 
   login(){
     // Validate
@@ -35,7 +45,12 @@ export class LoginComponent {
       },
       error: (error) => {
         this.isLoading.set(false);
-        this.errorMessage.set('Contraseña o usuario invalido');
+
+        if (error.status === 401)
+          this.errorMessage.set('Usuario o contraseña incorrectos');
+        else 
+          this.errorMessage.set('Error al iniciar sesión. Intenta de nuevo.');
+
         console.error('Error al iniciar sesion:', error);
       }
     });
