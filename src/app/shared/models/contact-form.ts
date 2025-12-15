f6c2d08c1f9e4bbe0e950e9816ef7003695915b6
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-15 13:37:39 -0600

    Make the contact form model fit the actual backend's model.

--- src/app/shared/models/contact-form.ts
+++ src/app/shared/models/contact-form.ts
@@ -1,7 +1,10 @@
-export interface ContactForm{
+export interface ContactForm {
+  id?: number;
   name: string;
   email: string;
   phone?: string;
   inquiryType: string;
   message: string;
+  submittedAt?: Date;
+  isRead?: boolean;
 }
