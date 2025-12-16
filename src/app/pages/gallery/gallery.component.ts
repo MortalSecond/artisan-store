c1913895b2528587a530147bd597520b3040c156
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-16 10:39:50 -0600

    Add loading spinner to the gallery page.

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,19 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { PaintingService } from '../../services/painting.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { GalleryItemComponent } from '../../shared/components/gallery/gallery-item.component';
+import { finalize } from 'rxjs';
+import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-gallery',
-  imports: [TranslateModule, GalleryItemComponent],
+  imports: [TranslateModule, GalleryItemComponent, LoadingSpinnerComponent],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
   private paintingsService = inject(PaintingService);
+  isLoading = signal(true);
 
-  paintings = toSignal(this.paintingsService.getAllPaintings(),{
+  paintings = toSignal(this.paintingsService.getAllPaintings().pipe(finalize(()=>this.isLoading.set(false))),{
     initialValue:[]
   });
 }
