3cd408899e47aabc69aba536035035be595c8c7d
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-16 02:18:02 -0600

    Add a loading spinner component.

---
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  imports: [TranslateModule],
+  templateUrl: './loading-spinner.component.html',
+  styleUrl: './loading-spinner.component.css'
+})
+export class LoadingSpinnerComponent {
+
+}
