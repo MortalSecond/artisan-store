d1dc2d5a5dded50c8c820e700e8092760d4f20e3
MortalSecond <irondiego_jkl@hotmail.com>
2026-01-12 15:23:53 -0600

    Undo environment variable change.
    I forgot to unstage the last file change in the commit, so the frontend is trying to access the API through my localhost instead of the actual API website URL.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:5051/api'
+    production: true,
+    apiUrl: 'https://artisanshopapi-production.up.railway.app/api'
 };
